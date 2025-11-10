69449d9a7e5995247f2e3b7cf222c8c634a2c582
Thauã <thaua23sl@gmail.com>
2025-11-09 23:24:51 -0300

    refri2
    Reorganized the code for handling active items and added initialization for the first active item.

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 let list = document.querySelectorAll('.item');
-
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
-
-//contador = 3
-//ativo = 0
 
 let count = list.length;
 let active = 0;
 
-next.onclick = () =>{
-    let activeOld = document.querySelector('.active');
-    activeOld.classList.remove('active');
-    active++;
-    list[active % count].classList.add('active');
-}
-prev.onclick = () =>{
-    let activeOld = document.querySelector('.active');
-    activeOld.classList.remove('active');
-    active--;
-    list[active % count].classList.add('active');
-}
+// Inicializa o primeiro item como ativo
+list[active].classList.add('active');
+next.onclick = () => {
+    list[active].classList.remove('active');
+    active = (active + 1) % count;
+    list[active].classList.add('active');
+};
+prev.onclick = () => {
+    list[active].classList.remove('active');
+    active = (active - 1 + count) % count;
+    list[active].classList.add('active');
+};
